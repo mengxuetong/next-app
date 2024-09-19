28be11bf864263cbcc22109c4c3f40907b299587
孟学桐 <mengxuetong@bjy.powerchina.cn>
2024-09-19 17:23:01 +0800

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,13 +2,16 @@
  * @Author: 孟学桐 mengxuetong@bjy.powerchina.cn
  * @Date: 2024-09-18 17:41:59
  * @LastEditors: 孟学桐 mengxuetong@bjy.powerchina.cn
- * @LastEditTime: 2024-09-18 18:14:46
+ * @LastEditTime: 2024-09-19 17:21:56
  * @FilePath: /test-app/app/home/page.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import type { Metadata } from 'next'
-
-export default function About(props) {
+interface IProps {
+    params: { id: string },
+    searchParams: { [key: string]: string | string[] | undefined }
+}
+export default function About(props: IProps) {
     return (
         <div>about page{JSON.stringify(props)}
             
